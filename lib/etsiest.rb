5a2e3ce12711efffae33e7db2082f88b10fe426c
Betty L. Hutchings <blhutch4@gmail.com>
2015-06-08 08:49:22 -0400

    First commit, kind of running.

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,26 @@
 require "etsiest/version"
+require "sinatra/base"
+require 'pry'
+
+require 'etsy'
+Etsy.api_key = 'cwo6c9n9159hsakqvz36lt8m'
+
 
 module Etsiest
-  # Your code goes here...
+  
+  class App < Sinatra::Base
+
+  # start the server on the command line
+  # in your browser: localhost:4567/search?q=
+
+    get '/etsy_search' do
+    # get the search parameter out
+    # etsy_search = make a request to Etsy API with the gem
+      @results = Etsy::Request.get('/listings/active', :includes =>
+             ['Images', 'Shop'], :keywords => params['q'])
+      erb :index
+    end
+
+    run! if app_file == $0
+  end
 end
